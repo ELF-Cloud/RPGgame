1ce8a43288c1f48207b5e2520e4507bf24506c73
OS <ttcenter@192.168.1.78>
2026-04-26 17:37:59 +0700

    Add player bow and arrow system

---
+++ Assets/Scripts/Player_Bow.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Player_Bow : MonoBehaviour
+{
+    public Transform launchPoint;
+    public GameObject arrowPrefab;
+
+    private Vector2 aimDirection = Vector2.right;
+
+    public float shootCooldown = .5f;
+    private float shootTimer;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        shootTimer -= Time.deltaTime;
+        
+        HandleAiming();
+
+        if(Input.GetButtonDown("Shoot") && shootTimer <= 0)
+        {
+            Shoot();
+        }
+
+    }
+
+    private void HandleAiming()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); 
+        float vertical = Input.GetAxis("Vertical");
+
+        if(horizontal != 0 || vertical != 0)
+        {
+            aimDirection = new Vector2(horizontal, vertical).normalized;
+        
+        }
+    }
+
+    public void Shoot()
+    {
+        
+          Arrow arrow = Instantiate(arrowPrefab, launchPoint.position, Quaternion.identity).GetComponent<Arrow>();
+          arrow.direction = aimDirection;
+          shootTimer = shootCooldown;
+        
+    }
+}
