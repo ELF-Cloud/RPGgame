6cc4ca317dd24efd8d24b66fc7cc00af229f7f82
YuGa <ddn11102005@gmail.com>
2026-05-05 22:28:02 +0700

    Upload full project to main2

---
+++ Assets/Scripts/Elevation_Entry.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Elevation_Entry : MonoBehaviour
+{
+    public Collider2D[] mountainCollider;
+    public Collider2D[] boundaryCollider;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            foreach (Collider2D mountain in mountainCollider)
+            {
+                mountain.enabled = false;
+            }
+
+            foreach (Collider2D boundary in boundaryCollider)
+            {
+                boundary.enabled = true;
+            }
+
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
+        }  
+    }
+}
